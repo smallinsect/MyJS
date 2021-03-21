f1ff4a482a337c5fe88d10e2270a000882781618
xuqiulin <513312514@qq.com>
2021-03-21 22:24:50 +0800

    web开发基础

---
+++ Web开发基础/test.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-21 21:28:31
+ * @LastEditTime: 2021-03-21 21:30:49
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \MyJS\Web开发基础\test.ts
+ */
+
+
+console.log(156*38);
+console.log(5928 / 60);
+console.log(5928 - 60*98);
+console.log(98 - 60);
---
+++ Web开发基础/web.md
@@ -0,0 +1,20 @@
+# Web开发基础
+
+- （结构）HTML负责创建页面结构
+- （样式）CSS负责美化页面结构
+- （行为）JavaScript负责让页面动起来，解锁更多动效果。
+
+# 快捷方式
+
+按英文的! 再按tab键，快速建立html页面。
+
+# 自动刷新浏览器
+
+VSCode安装LiveServer插件
+
+# 监视模式
+
+```powershell
+tsc --watch index.ts
+```
+
