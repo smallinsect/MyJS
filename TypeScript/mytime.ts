711a33cd3e202cb79e43de894f3bc74938cb9107
maichi <513312514@qq.com>
2021-04-02 17:05:28 +0800

    元组

--- TypeScript/mytime.ts
+++ TypeScript/mytime.ts
@@ -4,11 +4,12 @@
  * @Author: xuqiulin
  * @Date: 2021-04-02 16:09:41
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-04-02 16:12:00
+ * @LastEditTime: 2021-04-02 16:13:22
  */
 // 当前本地时间
 let date = new Date();
 console.log(date);
+date.getHours();
 // 当前时间戳
 let now = Date.now();
 console.log(now);
